43e685353748124c8732e6eef373b4ce23af1a1a
yuki <yukibochi1@gmail.com>
2019-08-09 13:24:42 +0900

    BasicParser整理

--- WorkFlow/ASTree/Parser/Impl/BasicParser.hpp
+++ WorkFlow/ASTree/Parser/Impl/BasicParser.hpp
@@ -15,10 +15,14 @@
 
 class BasicParser {
 public:
+    /// 演算子を格納します。
     wf::Operators operators = wf::Operators();
     
+    /// 式を表します。(1 + 3 * 3)...、実態はexprにあります。
     wf::Parser expr0 = wf::rule<wf::ast::Expression>();
     
+    /// 基本のノードを表します。
+    /// Int、String、Name、式など
     wf::Parser primary = wf::rule().ors({
         wf::Parser::integer(),
         wf::Parser::name(),
@@ -26,15 +30,19 @@ public:
         wf::rule<wf::ast::Expression>().skip("(").then(expr0).skip(")"),
     });
 
+    /// 式を表します。(1 + 3 * 3)...
     wf::Parser expr = expr0.expression(primary, operators);
     
+    /// 文を表します。実態はstatementにあります。
     wf::Parser statement0 = wf::rule();
     
+    /// ブロックを表します。
     wf::Parser block = wf::rule<wf::ast::BlockStem>()
         .skip("{").optional(statement0)
         .optionalRepeat(wf::rule().skipEol().optional(statement0))
         .skip("}");
     
+    /// 文を表します。(if while def...)
     wf::Parser statement = statement0.ors({
         wf::rule<wf::ast::IfStem>().skip("if").then(expr).then(block).optional(wf::rule().skip("else").then(block)),
         wf::rule<wf::ast::WhileStem>().skip("while").then(expr).then(block),
@@ -44,12 +52,13 @@ public:
         wf::rule<wf::ast::Calling>().then(wf::Parser::name()).skip("(").optional(expr).skip(")"),
     });
     
-    wf::Parser program = wf::rule().ors({statement, wf::rule()}).skipEol();
+    /// プログラムとしての一つのまとまりを表します。
+    wf::Parser program = wf::rule().ors({statement, wf::rule<wf::ast::NullStem>()}).skipEol();
 
     BasicParser() {
+        // MARK: - Add Operators -
         operators.add("=", 1,   wf::Associative::RIGHT);
         operators.add("==", 2,  wf::Associative::LEFT);
-        operators.add("+=", 2,  wf::Associative::LEFT);
         operators.add(">", 2,   wf::Associative::LEFT);
         operators.add("<", 2,   wf::Associative::LEFT);
         operators.add("+", 3,   wf::Associative::LEFT);
